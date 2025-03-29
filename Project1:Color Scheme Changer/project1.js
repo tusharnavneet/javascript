e6f4617a6163ca774f758871e1cd2c6a5e222fb7
tusharnavneet <116185952+tusharnavneet@users.noreply.github.com>
2025-03-29 10:16:46 +0000

    Project1: Color Scheme Changer Done

---
+++ Project1:Color Scheme Changer/project1.js
@@ -0,0 +1,25 @@
+const buttons=document.querySelectorAll('.button');
+const body=document.querySelector('body');
+
+//as queryselAll gives listnodes we can apply forEach for selecting all buttton
+buttons.forEach(function(button){
+
+    button.addEventListener('click',function(e){
+              //console.log(e);
+              //console.log(e.target);
+              if (e.target.id === 'grey') {
+                body.style.backgroundColor = e.target.id;
+              }
+              if (e.target.id === 'white') {
+                body.style.backgroundColor = e.target.id;
+              }
+              if (e.target.id === 'blue') {
+                body.style.backgroundColor = e.target.id;
+              }
+              if (e.target.id === 'yellow') {
+                body.style.backgroundColor = e.target.id;
+              }
+    }
+)
+    
+})
